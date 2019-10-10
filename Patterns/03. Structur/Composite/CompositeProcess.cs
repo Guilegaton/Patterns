6f6b4c0085d6c5df5677945a06425c542bd4bed5
Julii Vedkal <julii.vedkal@ssa.group>
2019-10-10 18:31:16 +0300

    [add} - composite

---
+++ Patterns/03. Structur/Composite/CompositeProcess.cs
@@ -0,0 +1,48 @@
+﻿using Patterns._03._Structur.Composite.Models;
+using Patterns.Interfaces;
+
+namespace Patterns._03._Structur.Composite
+{
+    public class CompositeProcess : IExampleService
+    {
+        #region Public Methods
+
+        public void StartExample()
+        {
+            var neuron1 = new Neuron("Neuron-1");
+            var neuron11 = new Neuron("Neuron-1-1");
+            var neuron12 = new Neuron("Neuron-1-2");
+            var neuron13 = new Neuron("Neuron-1-3");
+            var neuron14 = new Neuron("Neuron-1-4");
+
+            var neuron121 = new Neuron("Neuron-1-2-1");
+            var neuron122 = new Neuron("Neuron-1-2-2");
+            var neuron123 = new Neuron("Neuron-1-2-3");
+            var neuron124 = new Neuron("Neuron-1-2-4");
+
+            var neuron1241 = new Neuron("Neuron-1-2-4-1");
+            var neuron1242 = new Neuron("Neuron-1-2-4-2");
+            var neuron1243 = new Neuron("Neuron-1-2-4-3");
+            var neuron1244 = new Neuron("Neuron-1-2-4-4");
+
+            neuron1.ConnectTo(neuron11);
+            neuron1.ConnectTo(neuron12);
+            neuron1.ConnectTo(neuron13);
+            neuron1.ConnectTo(neuron14);
+
+            neuron12.ConnectTo(neuron121);
+            neuron12.ConnectTo(neuron122);
+            neuron12.ConnectTo(neuron123);
+            neuron12.ConnectTo(neuron124);
+
+            neuron124.ConnectTo(neuron1241);
+            neuron124.ConnectTo(neuron1242);
+            neuron124.ConnectTo(neuron1243);
+            neuron124.ConnectTo(neuron1244);
+
+            neuron1.DoWork();
+        }
+
+        #endregion Public Methods
+    }
+}
---
+++ Patterns/03. Structur/Composite/Models/Neuron.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Patterns._03._Structur.Composite.Models
+{
+    public class Neuron : IEnumerable<Neuron>
+    {
+        #region Public Constructors
+
+        public Neuron(string name)
+        {
+            Name = name;
+            Into = new List<Neuron>();
+            Out = new List<Neuron>();
+        }
+
+        #endregion Public Constructors
+
+        #region Public Properties
+
+        public string Name { get; private set; }
+
+        #endregion Public Properties
+
+        #region Private Properties
+
+        private List<Neuron> Into { get; set; }
+        private List<Neuron> Out { get; set; }
+
+        #endregion Private Properties
+
+        #region Public Methods
+
+        public void ConnectTo(Neuron other)
+        {
+            Out.Add(other);
+            other.Into.Add(this);
+        }
+
+        public void DoWork()
+        {
+            Console.WriteLine(Name);
+            foreach (var item in this)
+            {
+                item.DoWork();
+            }
+        }
+
+        public IEnumerator<Neuron> GetEnumerator()
+        {
+            return this.Out.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        #endregion Public Methods
+    }
+}
